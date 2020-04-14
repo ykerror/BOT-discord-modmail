04121d464af6504b006a2dea64f03f5cf40a3a30
raphael veauville <veauville.raphael@gmail.com>
2020-04-14 19:49:05 +0200

    ping => .js

--- commands/public/general.js
+++ commands/public/general.js
@@ -8,16 +8,16 @@ module.exports = (client)=>{
             let fleche = client.emojis.cache.find(emoji => emoji.name === "fleche");
 
             message.reply(`Voici les commandes enregistrer :\r\n 
-            ${fleche} **Les réseaux sociaux**\r\n 
-            ${g4} **${prefix}fb** Donne le lien de notre page facebook
-            ${g4} **${prefix}twitter** Donne le lien de notre page twitter
-            ${g4} **${prefix}insta** Donne le lien de notre page instagram
-            ${g4} **${prefix}utip** Donne le lien de notre page utip
-            ${g4} **${prefix}youtube** Donne le lien de notre chaine youtube
-            ${fleche} **Les commandes**\r\n 
-            ${g4} **${prefix}avatar** montre votre avatar au grand format
-            ${g4} **${prefix}pileouface** faites jouer le hasard !
-            ${g4} **${prefix}dés** lance un dés a 6 faces`);          
+        ${fleche} **Les réseaux sociaux**\r\n 
+        ${g4} **${prefix}fb** Donne le lien de notre page facebook
+        ${g4} **${prefix}twitter** Donne le lien de notre page twitter
+        ${g4} **${prefix}insta** Donne le lien de notre page instagram
+        ${g4} **${prefix}utip** Donne le lien de notre page utip
+        ${g4} **${prefix}youtube** Donne le lien de notre chaine youtube
+        ${fleche} **Les commandes**\r\n 
+        ${g4} **${prefix}avatar** montre votre avatar au grand format
+        ${g4} **${prefix}pileouface** faites jouer le hasard !
+        ${g4} **${prefix}dés** lance un dés a 6 faces`);          
         }
         if(message.content===prefix+"avatar"){
             message.reply(message.author.displayAvatarURL());
--- commands/public/ping.js
+++ commands/public/ping.js
@@ -1,6 +1,6 @@
 module.exports = (client,message) => {
     const prefix = '!'
-    client.on("message", function(message) {
+         client.on("message", function(message) {
         if(message.content===prefix+"ping"){
             let debut = Date.now();
             message.channel.send("Ping").then(async(m) => await m.edit(`Pong : ${Date.now()-debut}ms`));
