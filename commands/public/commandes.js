db8afe58f118ab6af1f7d3e6b818c4da38a21488
raphael veauville <veauville.raphael@gmail.com>
2020-04-14 20:22:12 +0200

    Add commandes.js

--- commands/public/commandes.js
+++ commands/public/commandes.js
@@ -1,6 +1,7 @@
 
 module.exports = (client)=>{
     const Discord =require('discord.js');
+    const user = message.author
     let fleche = client.emojis.cache.find(emoji => emoji.name === "fleche");
     let g4 = client.emojis.cache.find(emoji => emoji.name === "g4");
     const prefix = '!'
