dab358000446ff26623ce363825435ff4b3a668c
raphael veauville <veauville.raphael@gmail.com>
2020-04-15 11:22:09 +0200

    Ajout départ channel

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,7 @@
 module.exports = (client, member)=>{
 
     client.on('guildMemberRemove', member => {
-        
+        const departChanel = '675374925527253002'
         user = member.user
         member.guild.channels.cache.get(departChanel).send("**" + user.tag +"** vient de nous quitter. A bientôt l'ami ! :wave:");
     });
