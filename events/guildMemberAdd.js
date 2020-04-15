f13895febcae00e675f40ab69545648e3cf04520
raphael veauville <veauville.raphael@gmail.com>
2020-04-15 11:26:04 +0200

    Modif message bienvenue channel

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,7 @@ module.exports = (client, member)=>{
         NewUser = member.user;
         let attention = client.emojis.cache.find(emoji => emoji.name === "attention");
         member.guild.channels.cache.get(bienvenueChanel).send("__Salut __"+"<@" + NewUser + ">" +`__, bienvenue sur le serveur communautaire de la French Gaming Family Dev ! 🤗__ 🎉\r\n\r\n __**N'oublie pas d'aller dans le channel <#685912863428313140> pour pouvoir vérifier l'actualité sur le serveur \r\n${attention} Si tu veux pouvoir rejoindre notre équipe de devs n'hésite pas a envoyer un message privé a <@611468402263064577> !**__\r\n\r\n`); 
-        member.user.send("__Salut __"+"<@" + NewUser + ">" +`__, bienvenue sur le serveur communautaire de la French Gaming Family Dev ! 🤗__ 🎉\r\n\r\n __**N'oublie pas d'aller dans le channel <#685912863428313140> pour pouvoir vérifier l'actualité sur le serveur \r\n${attention} Si tu veux pouvoir rejoindre notre équipe de devs n'hésite pas a envoyer un message privé a <@611468402263064577> !**__\r\n\r\n`);
+        member.user.send("__Salut __"+"<@" + NewUser + ">" +`__, bienvenue sur le serveur communautaire de la French Gaming Family Dev ! 🤗__ 🎉\r\n\r\n __**N'oublie pas d'aller dans le channel <#685912863428313140> pour pouvoir vérifier l'actualité sur le serveur \r\n${attention} Si tu veux pouvoir rejoindre notre équipe de devs n'hésite pas a envoyer un message privé a Smaug !**__\r\n\r\n`);
     
     
     
