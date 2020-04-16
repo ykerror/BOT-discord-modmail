2b90f62e5b5fa721a060a3a29dde0227961e5215
raphael veauville <veauville.raphael@gmail.com>
2020-04-16 13:12:37 +0200

    Ping.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,11 +1,8 @@
 module.exports = (client, member)=>{
-    const bienvenueChanel ='688411985347477538'
-
+        const bienvenueChanel ='688411985347477538'
         NewUser = member.user;
         let attention = client.emojis.cache.find(emoji => emoji.name === "attention");
+        
         member.guild.channels.cache.get(bienvenueChanel).send("__Salut __"+"<@" + NewUser + ">" +`__, bienvenue sur le serveur communautaire de la French Gaming Family Dev ! 🤗__ 🎉\r\n\r\n __**N'oublie pas d'aller dans le channel <#685912863428313140> pour pouvoir vérifier l'actualité sur le serveur \r\n${attention} Si tu veux pouvoir rejoindre notre équipe de devs n'hésite pas a envoyer un message privé a <@611468402263064577> !**__\r\n\r\n`); 
         member.user.send("__Salut __"+"<@" + NewUser + ">" +`__, bienvenue sur le serveur communautaire de la French Gaming Family Dev ! 🤗__ 🎉\r\n\r\n __**N'oublie pas d'aller dans le channel <#685912863428313140> pour pouvoir vérifier l'actualité sur le serveur \r\n${attention} Si tu veux pouvoir rejoindre notre équipe de devs n'hésite pas a envoyer un message privé a Smaug !**__\r\n\r\n`);
-    
-    
-    
 }
