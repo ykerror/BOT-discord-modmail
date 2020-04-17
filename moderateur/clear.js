92307cf321950c2d5bf689dfc469b55e51eb0b65
raphael veauville <veauville.raphael@gmail.com>
2020-04-17 11:47:32 +0200

    Update img author and footer

--- moderateur/clear.js
+++ moderateur/clear.js
@@ -8,7 +8,7 @@ module.exports = (client,message) => {
                     //if(!count)return message.channel.send('Veuillez indiquer le nombre de message(s) a spprimer après la commande')
                     if(isNaN(count))return message.channel.send('Veuillez indiquer un nombre valide')
                     if(count < 1 || count > 100)return message.channel.send('Veuillez indiquer un nombre compris entre 0 et 100')
-                    message.channel.bulkDelete(parseInt(count))
+                    message.channel.bulkDelete(parseInt(count) + 1)
                 }
             } 
         })
