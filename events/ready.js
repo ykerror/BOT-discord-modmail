8544b4cd78d01885279f5ba367e67b7945a1c363
raphael veauville <veauville.raphael@gmail.com>
2020-05-13 20:31:09 +0200

    Update setPresence

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,9 @@
 module.exports = ()=>{
+        const {STATUS,MESSAGESTATUS} = require('./../config.js');
+        console.log(`${bot.user.username} is online!`);
+        bot.user.setStatus(STATUS);
+        bot.user.setActivity(MESSAGESTATUS)
+         
         console.log('BOT démarer avec succès')
     
 }
