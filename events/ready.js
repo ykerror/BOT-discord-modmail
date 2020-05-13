5b61e4c7c6a676a52a4ca5e2a4a0f98d457e1869
raphael veauville <veauville.raphael@gmail.com>
2020-05-13 20:32:56 +0200

    fix set presence

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 module.exports = ()=>{
         const {STATUS,MESSAGESTATUS} = require('./../config.js');
         console.log(`${bot.user.username} is online!`);
-        bot.user.setStatus(STATUS);
-        bot.user.setActivity(MESSAGESTATUS)
+        client.user.setStatus(STATUS);
+        client.user.setActivity(MESSAGESTATUS)
          
         console.log('BOT démarer avec succès')
     
