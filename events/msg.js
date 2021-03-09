5fc24d8322a8589c4261a39e8af7b3389445f87f
Raphael <veauville.raphael@gmail.com>
2021-03-09 17:47:14 +0100

    Update

--- events/msg.js
+++
@@ -1,8 +0,0 @@
-module.exports =  (client, message) => {
-    
-    
-    if (message.author.bot) return;
-    if (message.content.indexOf(settings.prefix) !== 0) return;
-  
-    const args
-  };
--- events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = (client)=>{
-        const {STATUS,MESSAGESTATUS} = require('./../config.js');
-        console.log(`${client.user.username} is online!`);
-        client.user.setStatus(STATUS);
-        client.user.setActivity(MESSAGESTATUS)
-         
-        console.log('BOT démarer avec succès')
-    
-}
